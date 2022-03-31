eaf4593c95e6d4c1adfc7bfff16bcaff2713cee3
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-03-31 14:32:00 -0300

    Ex 8 completed, ex 12 started

---
+++ Assets/Scripts/Ej08.cs
@@ -0,0 +1,83 @@
+﻿//Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+//el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+//valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+//mensaje de error "El día ingresado no es válido".
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej08 : MonoBehaviour
+{
+    public int dia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (dia<1 || dia >7)
+        //{
+        //    Debug.Log("El día ingresado no es válido");
+        //}else if (dia == 1)
+        //{
+        //    Debug.Log("Domingo");
+        //}
+        //else if (dia == 2)
+        //{
+        //    Debug.Log("Lunes");
+        //}
+        //else if (dia == 3)
+        //{
+        //    Debug.Log("Martes");
+        //}
+        //else if (dia == 4)
+        //{
+        //    Debug.Log("Miércoles");
+        //}
+        //else if (dia == 5)
+        //{
+        //    Debug.Log("Jueves");
+        //}
+        //else if (dia == 6)
+        //{
+        //    Debug.Log("Viernes");
+        //}
+        //else if (dia == 7)
+        //{
+        //    Debug.Log("Sábado");
+        //}}
+
+        switch (dia)
+        {            
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miércoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            case 6:
+                Debug.Log("Viernes");
+                break;
+            case 7:
+                Debug.Log("Sábado");
+                break;
+            default:
+                Debug.Log("El día ingresado no es válido");
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SwitchConString.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchConString : MonoBehaviour
+{
+    public string curso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (curso)
+        {
+            case "A":
+                Debug.Log("33 alumnos");
+                break;
+            case "B":
+                Debug.Log("31 alumnos");
+                break;
+            case "C":
+                Debug.Log("30 alumnos");
+                break;
+            default:
+                Debug.Log("No existe ese curso");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
